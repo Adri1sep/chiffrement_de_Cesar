72b98cb24e1d3b068f63c4e1daaa43e7d22b5f7c
Cyriltrn <traineaucyril8@gmail.com>
2025-05-20 14:32:47 -0400

    Ajout en-tête

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#test
+'''---------------Mini-Projet A - Chiffrement de César----------------------
+
+    - Killian Berteaud
+    - Adrien Sepierre
+    - Cyril Traineau
+
+Cours MGA802, Session Été 2025
+'''
