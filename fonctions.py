54f983c0a8ebb6acc55ef043a11db67d47949e01
adri1sep <sepierreadri1@gmail.com>
2025-05-20 15:32:05 -0400

    main

--- fonctions.py
+++ fonctions.py
@@ -1,64 +1,22 @@
-"""Ce module permet de fournir des fonctions pour le chiffrement de C
-Il est écrit par Marlene Sanjose dans le cadre du cours MGA802
+"""Ce module permet de fournir des fonctions pour le chiffrement de César
 """
 
+import unicodedata, os
 
-def lire_liste_mots(fichier="mots_pendu.txt", dossier="./ressources"):
-    import os
-
-    # teste si le fichier existe
-    full_filename = os.path.join(dossier,fichier)
-    if not os.path.isfile(full_filename):
-        raise RuntimeError(f'Je ne trouve pas le fichier {full_filename} !')
-
-    # Ouvre le fichier contenant les mots en mode lecture
-    with open(full_filename, 'r', encoding='utf8') as f:
-        # Lire le contenu du fichier
-        words = f.read()
-
-    # Transforme la chaine de caracteres en liste
-    # le saut de ligne sert de separateur de champs
-    word_list = words.split('\n')
-
-    # retourne la liste de mots
-    return word_list
-
-
+#enlever les accents
 def enlever_caracteres_speciaux(mot):
-    import unicodedata
-
     # Solution obtenue sur https://www.geeksforgeeks.org/how-to-remove-string-accents-using-python-3/
     normalized_word = unicodedata.normalize('NFKD',mot)
     return ''.join([char for char in normalized_word if not unicodedata.combining(char)])
 
-
-def ouverture_fichier(fichier="texte_code.txt"):
-    # teste si le fichier existe
-    """full_filename = os.path.join(dossier,fichier)
-    if not os.path.isfile(full_filename):
-        raise RuntimeError(f'Je ne trouve pas le fichier {full_filename} !')"""
-
-# Ouvrir le fichier en mode lecture
-    with open("texte_code.txt", "r", encoding = "utf-8") as fio:
-# Lire le contenu du fichier
-        contenu = fio.read()
-        print(contenu)
-
-# On crée un fichier txt et on écrit dedans
-with open("message_encrypte.txt", "w") as fio:
-    contenu=""
-    fio.write(contenu)
-
-ouverture_fichier()
-
-
-alphabet = string.ascii_lowercase
-
-print(alphabet)
-
-alphabet.find("t")
-
-new_character = alphabet[8]
-
-print(new_character)
-
+#ouvrir le fichier et renvoyer la liste
+def lire_fichier(fichier):
+    #tester si le fichier existe
+    if not os.path.isfile(fichier):
+        raise RuntimeError(f'Je ne trouve pas le fichier {fichier} !')
+    #lire le fichier
+    f = open(fichier, "r", encoding='utf-8')
+    texte_fichier=enlever_caracteres_speciaux(f.read())
+    liste=list(texte_fichier)
+    f.close()
+    return liste
--- main.py
+++ main.py
@@ -10,33 +10,3 @@ Cours MGA802, Session Été 2025
 
 #PROGRAMME PRINCIPAL
 
-import string
-import os
-
-def ouverture_fichier(dossier, fichier="message.txt"):
-    # teste si le fichier existe
-    full_filename = os.path.join(dossier,fichier)
-    if not os.path.isfile(full_filename):
-        raise RuntimeError(f'Je ne trouve pas le fichier {full_filename} !')
-
-# Ouvrir le fichier en mode lecture
-    with open("message.txt", "r", encoding = "utf-8") as fio:
-# Lire le contenu du fichier
-        contenu = fio.read()
-
-# On crée un fichier txt et on écrit dedans
-with open("message_encrypte.txt", "w") as fio:
-    contenu=""
-    fio.write(contenu)
-
-
-alphabet = string.ascii_lowercase
-
-print(alphabet)
-
-alphabet.find("t")
-
-new_character = alphabet[8]
-
-print(new_character)
-
