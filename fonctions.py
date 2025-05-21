742445698722e39c69613c5b8693910a8df13eaa
Test <kilianberteaudca@gmail.com>
2025-05-21 13:38:45 -0400

    Module

--- fonctions.py
+++ fonctions.py
@@ -9,7 +9,7 @@ def enlever_caracteres_speciaux(mot):
     normalized_word = unicodedata.normalize('NFKD',mot)
     return ''.join([char for char in normalized_word if not unicodedata.combining(char)])
 
-#ouvrir le fichier et renvoyer la liste, exemple lire_fichier("message.txt")
+#ouvrir le fichier et renvoyer la liste, exemple lire_fichier("texte_code.txt")
 def lire_fichier(fichier):
     #tester si le fichier existe
     if not os.path.isfile(fichier):
@@ -36,30 +36,28 @@ def ecrire_fichier(contenu, fichier):
     f.close()
 
 #Fonction qui crypte le texte en entrée:
-def cryptage(liste, cle_de_cryptage):           #Fonction qui crypte le texte avec le chiffrement de César
-    alphabet = string.ascii_lowercase           #On créé une chaîne de caractères qui contient l'alphabet
-    if cle_de_cryptage < 0 or cle_de_cryptage > 25:
-        cle_de_cryptage %= 26
-    for indice in range(len(liste)):            #On parcourt la liste de mots à crypter
-        if liste[indice].lower() in alphabet:           #Si la lettre est dans l'alphabet, permet de ne pas crypter les ponctuations
-            index = alphabet.find(liste[indice].lower())    #On récupère la position dans l'alphabet de la lettre issu de la liste
-            # print("Indice dans la liste: ", indice, "Lettre", liste[indice], "Index alphabet", index)
-            decalage = index + cle_de_cryptage      #On récupère le décalage souhaité en fonction de la clé de cryptage
-            if decalage > 25:                       #Les indices de l'alphabet vont de 0 à 25, on veut rester dans cette marge
-                decalage %= 26                      #En retranchant 26 on revient au début de l'alphabet
-            # elif decalage < -26:                    #Si la clé est négative, décalage vers la gauche dans l'alphabet
-            #     decalage = decalage + 25            #On rajoute 25, pour rester dans l'alphabet
-            new_character = alphabet[decalage]      #La lettre cryptée est prise dans l'alphabet avec sa nouvelle position
-            liste[indice] = new_character           #La lettre initiale est remplacée par la lettre cryptée.
-    texte_crypte = ""                               #On créé une chaîne de caractères vides destiné à recevoir le msg
-    for i in liste:                                 #On parcourt notre liste
-        texte_crypte += i                           #Chaque caractère de la liste est ajouté à notre texte
-    return texte_crypte                             #On retourne la chaine cryptée
+def cryptage(liste, cle_de_cryptage):
+    # texte = "motdepasse"
+    # texte_chiffre = list(texte)
+    # cle_de_chiffrage = 5
+    alphabet = string.ascii_lowercase
+    # print("alphabet: ", alphabet)
+
+    for indice in range(len(liste)):
+        # print("indice", indice)
+        # print("texte_chiffre[indice]", texte_chiffre[indice])
+        index = alphabet.find(liste[indice])
+        # print("Index", index)
+        decalage = index + cle_de_cryptage
+        if decalage > 25:
+            decalage = decalage - 26
+        elif decalage < -26:
+            decalage = decalage + 25
 
         new_character = alphabet[decalage]
         # print("new_character", new_character)
         liste[indice] = new_character
-
+    return liste
 
 def decryptage(cle,fichier="message_encrypte.txt"):
     alphabet = string.ascii_lowercase
